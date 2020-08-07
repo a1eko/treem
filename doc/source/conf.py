c0253af47ee2d2a877eff3f2d519de998c432fc8
Alexander Kozlov <akozlov@kth.se>
2020-08-07 14:17:35 +0200

    20200807

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Alexander Kozlov (KTH, Stockholm)'
 author = 'Alexander Kozlov (KTH, Stockholm)'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.0'
+release = '1.0.0a'
 
 
 # -- General configuration ---------------------------------------------------
