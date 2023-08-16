f118aef249a3dce1607608946a2deba3ce344c7d
Alexander Kozlov <akozlov@kth.se>
2023-08-16 18:16:46 +0200

    20230816

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = 'treem'
-copyright = '2020, Alexander Kozlov (KTH, Stockholm)'
+copyright = '2023, Alexander Kozlov (KTH, Stockholm)'
 author = 'Alexander Kozlov (KTH, Stockholm)'
 
 # The full version, including alpha/beta/rc tags
