1d5886c60a241452ff0596fa0269d95586e75737
Alexander Kozlov <akozlov@kth.se>
2025-11-11 07:51:46 +0100

    Code refactoring

--- treem/commands/modify.py
+++ treem/commands/modify.py
@@ -11,17 +11,146 @@ from treem.io import SWC
 from treem.utils.geom import rotation
 
 
+def _scale_radii(morph, nodes, scale_radius):
+    """Scales radii."""
+    scale = np.abs(scale_radius)
+    for node in nodes:
+        sec = list(node.section())
+        radii = morph.radii(sec)
+        radii *= scale
+
+
+def _scale_coords(morph, nodes, scale_coords):
+    """Scales X,Y,Z coordinates."""
+    scale = np.abs(scale_coords)
+    for node in nodes:
+        sec = list(node.section())
+        head = sec[0].coord().copy()
+        tail = sec[-1].coord().copy()
+        coords = morph.coords(sec)
+        coords *= np.array(scale)
+        shift = head - sec[0].coord()
+        coords += shift
+        for child in sec[-1].siblings:
+            shift = sec[-1].coord() - tail
+            morph.translate(shift, child)
+
+
+def _jitter_coords(morph, nodes, jitter, rng, per_section=False):
+    """Adds random jitter to X,Y,Z coordinates."""
+    for node in nodes:
+        sec = list(node.section())
+        if len(sec) > 1:
+            head = sec[0].coord().copy()
+            tail = sec[-1].coord().copy()
+            length = morph.length(sec[1:])
+            coords = morph.coords(sec)
+            if not per_section:
+                rnd = rng.uniform(-1, 1, np.shape(coords))
+                coords += jitter * rnd
+            else:
+                xlen = 0
+                rnd = rng.uniform(-1, 1, 3)
+                for node in sec:
+                    xlen += node.length()
+                    vec = jitter * rnd * xlen / length
+                    morph.move(vec, node)
+            scale = length / morph.length(sec[1:])
+            coords *= scale
+            shift = head - sec[0].coord()
+            coords += shift
+            for child in sec[-1].siblings:
+                shift = sec[-1].coord() - tail
+                morph.translate(shift, child)
+
+
+def _twist_branches(morph, nodes, twist, rng):
+    """Rotates branches by random angle."""
+    for node in nodes:
+        axis = node.coord() - node.parent.coord()
+        angle = twist * rng.uniform(-1, 1) * math.pi / 180
+        morph.rotate(axis, angle, node)
+
+
+def _stretch_sections(morph, nodes, stretch):
+    """Straighten sections by relative factor."""
+    for node in nodes:
+        sec = list(node.section())
+        if len(sec) > 1:
+            head = sec[0].coord().copy()
+            tail = sec[-1].coord().copy()
+            length = morph.length(sec[1:])
+            coords = morph.coords(sec)
+            vdir = coords.mean(axis=0) - head
+            vdir /= np.linalg.norm(vdir)
+            for secnode in sec[1:]:
+                coord = secnode.coord()
+                coord += vdir * np.linalg.norm(coord - head) * stretch
+            scale = length / morph.length(sec[1:])
+            coords *= scale
+            shift = head - sec[0].coord()
+            coords += shift
+            for child in sec[-1].siblings:
+                shift = sec[-1].coord() - tail
+                morph.translate(shift, child)
+
+
+def _smooth_sections(morph, nodes, smooth):
+    """Smooth sections iteratively by low-pass filtering."""
+    for node in nodes:
+        sec = list(node.section())
+        if len(sec) > 2:
+            head = sec[0].coord().copy()
+            tail = sec[-1].coord().copy()
+            length = morph.length(sec[1:])
+            coords = morph.coords(sec)
+            for _ in range(smooth):
+                for secnode in sec[-1::-1]:
+                    coord = secnode.coord()
+                    coord += secnode.parent.coord()
+                scale = length / morph.length(sec[1:])
+                coords *= scale
+                shift = head - sec[0].coord()
+                coords += shift
+            for child in sec[-1].siblings:
+                shift = sec[-1].coord() - tail
+                morph.translate(shift, child)
+
+
+def _swap_branches(morph, node1, node2):
+    """Swap two branches."""
+    parent1 = node1.parent
+    parent2 = node2.parent
+    dir1 = node1.coord() - parent1.coord()
+    dir2 = node2.coord() - parent2.coord()
+    tree1 = morph.copy(node1)
+    tree2 = morph.copy(node2)
+    coord1 = tree1.root.coord().copy()
+    coord2 = tree2.root.coord().copy()
+    tree1.translate(coord2 - coord1)
+    axis, angle = rotation(dir1, dir2)
+    tree1.rotate(axis, angle)
+    tree2.translate(coord1 - coord2)
+    axis, angle = rotation(dir2, dir1)
+    tree2.rotate(axis, angle)
+    morph.graft(tree1, parent2)
+    morph.graft(tree2, parent1)
+    morph.prune(node1)
+    morph.prune(node2)
+
+
 def modify(args):
     """Modifies selected parts of morphology reconstruction."""
-    # pylint: disable=too-many-locals
-    # pylint: disable=too-many-branches
-    # pylint: disable=too-many-statements
     morph = Morph(args.file)
+
+    # collect nodes by given ids, default to section start nodes
     if args.ids:
         nodes = filter(lambda x: x.ident() in args.ids, morph.root.walk())
     else:
         sections = chain.from_iterable(x.sections() for x in morph.stems())
-        nodes = chain(x[0] for x in sections)
+        nodes = chain(sec[0] for sec in sections)
+
+    # filter nodes by attributes
     types = args.type if args.type else SWC.TYPES
     nodes = filter(lambda x: x.type() in types, nodes)
     if args.order:
@@ -30,26 +159,12 @@ def modify(args):
         nodes = filter(lambda x: x.breadth() in args.breadth, nodes)
     nodes = list(nodes)
 
+    # manipulate morphology at specified nodes
     if args.scale_radius:
-        scale = np.abs(args.scale_radius)
-        for node in nodes:
-            sec = list(node.section())
-            radii = morph.radii(sec)
-            radii *= scale
+        _scale_radii(morph, nodes, args.scale_radius)
 
     if args.scale:
-        scale = np.abs(args.scale)
-        for node in nodes:
-            sec = list(node.section())
-            head = sec[0].coord().copy()
-            tail = sec[-1].coord().copy()
-            coords = morph.coords(sec)
-            coords *= np.array(scale)
-            shift = head - sec[0].coord()
-            coords += shift
-            for child in sec[-1].siblings:
-                shift = sec[-1].coord() - tail
-                morph.translate(shift, child)
+        _scale_coords(morph, nodes, args.scale)
 
     if args.seed:
         rng = np.random.default_rng(seed=args.seed)
@@ -57,106 +172,28 @@ def modify(args):
         rng = np.random.default_rng(0)
 
     if args.jitter:
-        for node in nodes:
-            sec = list(node.section())
-            if len(sec) > 1:
-                head = sec[0].coord().copy()
-                tail = sec[-1].coord().copy()
-                length = morph.length(sec[1:])
-                coords = morph.coords(sec)
-                if not args.sec:
-                    rnd = rng.uniform(-1, 1, np.shape(coords))
-                    coords += args.jitter * rnd
-                else:
-                    xlen = 0
-                    rnd = rng.uniform(-1, 1, 3)
-                    for node in sec:
-                        xlen += node.length()
-                        vec = args.jitter * rnd * xlen / length
-                        morph.move(vec, node)
-                scale = length / morph.length(sec[1:])
-                coords *= scale
-                shift = head - sec[0].coord()
-                coords += shift
-                for child in sec[-1].siblings:
-                    shift = sec[-1].coord() - tail
-                    morph.translate(shift, child)
+        per_section = args.sec if args.sec else False
+        _jitter_coords(morph, nodes, args.jitter, rng, per_section=per_section)
 
     if args.twist:
-        for node in nodes:
-            axis = node.coord() - node.parent.coord()
-            angle = args.twist * rng.uniform(-1, 1) * math.pi / 180
-            morph.rotate(axis, angle, node)
+        _twist_branches(morph, nodes, args.twist, rng)
 
     if args.stretch:
-        for node in nodes:
-            sec = list(node.section())
-            if len(sec) > 1:
-                head = sec[0].coord().copy()
-                tail = sec[-1].coord().copy()
-                length = morph.length(sec[1:])
-                coords = morph.coords(sec)
-                vdir = coords.mean(axis=0) - head
-                vdir /= np.linalg.norm(vdir)
-                for secnode in sec[1:]:
-                    coord = secnode.coord()
-                    coord += vdir * np.linalg.norm(coord - head) * args.stretch
-                scale = length / morph.length(sec[1:])
-                coords *= scale
-                shift = head - sec[0].coord()
-                coords += shift
-                for child in sec[-1].siblings:
-                    shift = sec[-1].coord() - tail
-                    morph.translate(shift, child)
+        _stretch_sections(morph, nodes, args.stretch)
 
     if args.smooth:
-        for node in nodes:
-            sec = list(node.section())
-            if len(sec) > 2:
-                head = sec[0].coord().copy()
-                tail = sec[-1].coord().copy()
-                length = morph.length(sec[1:])
-                coords = morph.coords(sec)
-                for _ in range(args.smooth):
-                    for secnode in sec[-1::-1]:
-                        coord = secnode.coord()
-                        coord += secnode.parent.coord()
-                    scale = length / morph.length(sec[1:])
-                    coords *= scale
-                    shift = head - sec[0].coord()
-                    coords += shift
-                for child in sec[-1].siblings:
-                    shift = sec[-1].coord() - tail
-                    morph.translate(shift, child)
+        _smooth_sections(morph, nodes, args.smooth)
 
     if args.swap:
         rng.shuffle(nodes)
-        #for node1, node2 in zip(nodes[:-1:2], nodes[1::2]):
-        # swap 2 nodes at a time
-        for node1, node2 in zip(nodes[:1], nodes[1:]):
-            parent1 = node1.parent
-            parent2 = node2.parent
-            dir1 = node1.coord() - parent1.coord()
-            dir2 = node2.coord() - parent2.coord()
-            tree1 = morph.copy(node1)
-            tree2 = morph.copy(node2)
-            coord1 = tree1.root.coord().copy()
-            coord2 = tree2.root.coord().copy()
-            tree1.translate(coord2 - coord1)
-            axis, angle = rotation(dir1, dir2)
-            tree1.rotate(axis, angle)
-            tree2.translate(coord1 - coord2)
-            axis, angle = rotation(dir2, dir1)
-            tree2.rotate(axis, angle)
-            morph.graft(tree1, parent2)
-            morph.graft(tree2, parent1)
-            morph.prune(node1)
-            morph.prune(node2)
+        node1, node2 = nodes[:2]
+        _swap_branches(morph, node1, node2)
 
     if args.prune and not args.swap:
         for node in nodes:
             morph.prune(node)
 
+    # renumber nodes in restructured morphology
     if args.prune or args.swap:
         morph = Morph(data=morph.data)
 
