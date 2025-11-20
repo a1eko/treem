25063c10d7819ebbe86f927a7157197aaaa9f8f2
Alexander Kozlov <akozlov@kth.se>
2025-11-20 09:07:45 +0100

    Test coverage

--- tests/test_cmd_view.py
+++ tests/test_cmd_view.py
@@ -3,11 +3,6 @@
 import os
 import subprocess
 
-import matplotlib.pyplot as plt
-
-from treem import Morph
-from treem.utils.plot import plot_neuron, plot_points, plot_section
-
 
 def test_png(tmp_path):
     """Tests for common plot."""
@@ -99,32 +94,3 @@ def test_proj_yz(tmp_path):
     assert proc.returncode == 0
     assert stdout == ''
     assert stderr == ''
-
-
-def test_plot_neuron(tmp_path):
-    """Tests plot_neuron."""
-    os.chdir(os.path.dirname(__file__) + '/data')
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    morph = Morph('pass_simple_branch.swc')
-    plot_neuron(ax, morph)
-
-
-def test_plot_points(tmp_path):
-    """Tests plot_points."""
-    os.chdir(os.path.dirname(__file__) + '/data')
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    morph = Morph('pass_simple_branch.swc')
-    ids = [node.ident() for node in morph.root.walk()]
-    plot_points(ax, morph, ids, show_id=True)
-
-
-def test_plot_section(tmp_path):
-    """Tests plot_neuron."""
-    os.chdir(os.path.dirname(__file__) + '/data')
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    morph = Morph('pass_simple_branch.swc')
-    node = morph.root.siblings[0]
-    plot_section(ax, node, morph.data)
--- tests/test_geom.py
+++ tests/test_geom.py
@@ -1,8 +1,20 @@
 """Testing module geom."""
 
 import numpy as np
+from numpy.testing import assert_array_almost_equal
 
-from treem.utils.geom import rotation
+from treem.utils.geom import fibonacci_sphere, rotation, rotation_matrix, sample
+
+
+def test_fibonacci_sphere():
+    """Tests fibonacci_sphere."""
+    N = 1000
+    points = fibonacci_sphere(npoints=N)
+    expected_shape = (N, 3)
+    assert points.shape == expected_shape
+    magnitudes_squared = np.sum(points**2, axis=1)
+    expected_magnitudes = np.ones(N)
+    assert_array_almost_equal(magnitudes_squared, expected_magnitudes, decimal=10)
 
 
 def test_tree_rotation():
@@ -13,3 +25,35 @@ def test_tree_rotation():
     expected_scalar = 0.0
     assert np.allclose(result_vec, expected_vec)
     assert np.isclose(result_scalar, expected_scalar)
+    vecx = [1, 0, 0]
+    vecy = [0, 1, 0]
+    vecz = [0, 0, 1]
+    result_vec, result_scalar = rotation(vecx, vecy)
+    expected_vec = np.array(vecz)
+    expected_scalar = np.pi / 2
+    assert np.allclose(result_vec, expected_vec)
+    assert np.isclose(result_scalar, expected_scalar)
+
+
+def test_rotation_matrix():
+    """Tests rotation_matrix."""
+    vec = [1, 0, 0]
+    rot = np.pi / 2
+    result_matrix = rotation_matrix(vec, rot)
+    expected_matrix = np.array([[1., 0., 0.],
+                                [0., 0., -1.],
+                                [0., 1., 0.]])
+    assert np.allclose(result_matrix, expected_matrix)
+
+
+def test_sample():
+    """Tests point interpolation."""
+    points = np.array([[0, 0, 0, 1],
+                       [3, 0, 0, 1]])
+    expected_sample = np.array([[0., 0., 0., 1.],
+                                [1., 0., 0., 1.],
+                                [2., 0., 0., 1.],
+                                [3., 0., 0., 1.]])
+    result_sample = sample(points, 4)
+    assert np.allclose(result_sample, expected_sample)
+
